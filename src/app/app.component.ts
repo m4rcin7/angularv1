cd3f450da3afdadfcaa51f26c5d5aa6f90feea3c
mkpage1337 <mrku1337@gmail.com>
2025-01-28 09:13:20 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./components/header/header.component";
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
