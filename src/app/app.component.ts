261606947e72f6d2fd6849781927af681145dbed
mkpage1337 <mrku1337@gmail.com>
2025-01-28 08:47:56 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular1';
-}
+export class AppComponent {}
