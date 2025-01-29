ec71afc58a6dede5776bf2e3369a8f700fa615da
mkpage1337 <mrku1337@gmail.com>
2025-01-29 12:15:24 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponent } from './components/header/header.component';
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [ HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
