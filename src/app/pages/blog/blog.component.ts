55c060f6cfed389201d87e54d947e168f2932937
mkpage1337 <mrku1337@gmail.com>
2025-01-29 09:39:33 +0100

    commit

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
