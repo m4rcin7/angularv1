4bf9ee4e2b0b065897fc5960661f5b3c312dc42b
mkpage1337 <mrku1337@gmail.com>
2025-01-31 08:54:47 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { routes } from './app.routes';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { RouterModule } from '@angular/router';
+// import { routes } from './app.routes';
+// import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
-  providers: [],
-  bootstrap: [],
-})
-export class AppModule {}
+// @NgModule({
+//   declarations: [],
+//   imports: [BrowserModule, RouterModule.forRoot(routes)],
+//   providers: [],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
