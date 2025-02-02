5ebbe7456f2e742e1b72ba1e25854cb299f539a9
mkpage1337 <mrku1337@gmail.com>
2025-02-02 11:48:18 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RouterModule, FormsModule],
+  imports: [HeaderComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
